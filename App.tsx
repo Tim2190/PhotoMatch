99d66a2d47c1fac8948e339a0d981af024c47d04
Tim2190 <9189920ts@gmail.com>
2025-12-09 16:49:34 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   'https://nugixzapgicswhtuaeki.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51Z2l4emFwZ2ljc3dodHVhZWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NzEzMjAsImV4cCI6MjA0OTM0NzMyMH0.kGevgEdevKM4enF33Hahjg_AnqeKNFxWxV7kh9YRUeY'
+  'sb_publishable_kGevgEdevKM4enF33Hahjg_AnqeKNFx'
 );
 
 function App() {
