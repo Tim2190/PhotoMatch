ff94aa7d7a7c590e2d147ada08f4341783d125b0
Tim2190 <9189920ts@gmail.com>
2025-12-09 16:49:04 +0500

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://nugixzapgicswhtuaeki.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51Z2l4emFwZ2ljc3dodHVhZWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NzEzMjAsImV4cCI6MjA0OTM0NzMyMH0.kGevgEdevKM4enF33Hahjg_AnqeKNFxWxV7kh9YRUeY';
+const supabaseAnonKey = 'sb_publishable_kGevgEdevKM4enF33Hahjg_AnqeKNFx';
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
